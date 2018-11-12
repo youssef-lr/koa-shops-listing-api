df21862046e4d0d204d9721e9c8353eaf6c945c3
Youssef <youssef.lourayad@gmail.com>
2018-11-12 12:45:18 +0100

    bug fix when retreiving favorites

--- server/src/modules/shop/tasks/get-favorites-task.js
+++ server/src/modules/shop/tasks/get-favorites-task.js
@@ -4,6 +4,7 @@ module.exports = async (userId) => {
   const shops = await knex('shops')
     .join('shops_likes', 'shops.id', '=', 'shops_likes.shop_id')
     .where('shops_likes.user_id', userId)
+    .where('like_status', 'liked')
     .select('shops.*');
 
   return shops;
