f1591638ec2607ad5e5c0c4c66b17b910a49a60d
Youssef <youssef.lourayad@gmail.com>
2018-11-12 12:21:01 +0100

    added thumbnails and static files middleware

--- server/src/app.js
+++ server/src/app.js
@@ -2,6 +2,8 @@ const Koa = require('koa');
 const bodyParser = require('koa-bodyparser');
 const jwt = require('koa-jwt');
 const cors = require('@koa/cors');
+const serve = require('koa-static');
+const path = require('path');
 
 const app = new Koa();
 const PORT = process.env.PORT || 3000;
@@ -21,10 +23,13 @@ app.use((ctx, next) => next().catch((err) => {
   }
 }));
 
-app.use(jwt({ secret: 'secret' }).unless({ path: [/^\/auth/] }));
+app.use(jwt({ secret: 'secret' }).unless({ path: [/^\/(auth|images)/] }));
 
 app.use(authRoutes.routes());
 app.use(shopRoutes.routes());
+
+const imagesDir = path.join(__dirname, 'storage');
+app.use(serve(imagesDir));
 
 const server = app.listen(PORT, () => {
   /* eslint-disable-next-line */
--- server/src/database/seeds/shops_seed.js
+++ server/src/database/seeds/shops_seed.js
@@ -1,10 +1,11 @@
 exports.seed = async (knex) => {
   await knex('shops').del();
 
-  const names = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N'];
+  const names = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I'];
 
   const shops = names.map(name => ({
-    name,
+    name: `Shop ${name}`,
+    image: `store${name}.jpg`,
   }));
 
   return knex('shops').insert(shops);
